bf9eeed1ba3c21e0272d9510929aed70bd6db8a2
nikhiluk <nikhilsharma0304@gmail.com>
2024-05-10 10:20:48 +0100

    Update RqChallengeApplication.java

--- src/main/java/com/example/rqchallenge/RqChallengeApplication.java
+++ src/main/java/com/example/rqchallenge/RqChallengeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RqChallengeApplication {
 
     public static final String MY_TOKEN="ghp_seO7JSoNqZ1Jlsafyj4VkNlibNtjU80iPfV5"
+    public static final String AZURE_CONNECTION_STRING = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://0.0.0.0:64625/devstoreaccount1";
 
     public static void main(String[] args) {
         SpringApplication.run(RqChallengeApplication.class, args);
