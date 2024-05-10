e0dd5c7079771d57cd4fd32b2fe39f53289aefae
nikhiluk <nikhilsharma0304@gmail.com>
2024-05-10 08:20:10 +0100

    Update RqChallengeApplication.java

--- src/main/java/com/example/rqchallenge/RqChallengeApplication.java
+++ src/main/java/com/example/rqchallenge/RqChallengeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RqChallengeApplication {
 
     public static final String MY_TOKEN="ghp_seO7JSoNqZ1Jlsafyj4VkNlibNtjU80iPfV5"
-
+    public static final String ANOTHER_TOKEN="ghp_seO7JSoNqZ1Jlsafyj4VkNlibNtjU80iPfV5"
     public static void main(String[] args) {
         SpringApplication.run(RqChallengeApplication.class, args);
     }
